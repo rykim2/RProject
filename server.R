a20dc41b401378f9f4fa8ecd499d224d70a93b60
rykim2 <rykim2@usfca.edu>
2017-05-07 18:31:45 -0700

    Added graph

--- server.R
+++ server.R
@@ -8,12 +8,13 @@ shinyServer(
     selectedData <- function(input1, input2){
       temp <- final.costOfLiving[c(which(final.costOfLiving$City == input1), 
                                  which(final.costOfLiving$City == input2)), as.numeric(input$radio)]
-      # city1 <- temp[1]
-      # city2 <- temp[2]
+      result <- c(temp[1], temp[2])
+      return(result)
+  
     }
     
     values <- reactive(selectedData(input$selectBar, input$selectBar2))
-    
+
     
     output$text1 <- renderText({ 
       paste("You have selected", input$selectBar)
@@ -24,16 +25,15 @@ shinyServer(
     })
     
     output$text3 <- renderText({
-      values()
-    })
-    
-    output$text4 <- renderText({
-      paste("From the data column:", input$radio)
+      values()[1]
     })
 
     graph <- function(input1){
       ggplot(final.costOfLiving, aes(final.costOfLiving[[as.numeric(input$radio)]])) +
         geom_density(color = "pink", fill = "pink")
+        # geom_vline(xintercept =  , color = "red") +
+        # geom_vline(xintercept = city2, color = "yellow")
+      
     }
     
     
